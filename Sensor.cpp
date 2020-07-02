b857b6dc34d64001882e81098a4c1bb0745bbf89
James-S-007 <jspringer8@gatech.edu>
2020-07-02 14:50:37 -0400

    Added error detection for sensor reading

--- Sensor.cpp
+++ Sensor.cpp
@@ -27,28 +27,37 @@ Sensor::Sensor(uint8_t addy){
 
 
 int Sensor::readTemp(uint8_t registerAddy){               //returns 100*temp (for greater speed)
-  int errTotal = 0;
+  uint8_t errCount = 0;
+  int errTotal = 1;
   uint8_t LSB;
   uint8_t MSB;
   uint8_t PEC;
-  errTotal += I2c._start();
-  errTotal += I2c._sendAddress(this->address<<1);              //left shifted with W bit (0)
-  errTotal += I2c._sendByte(registerAddy);                  
-  errTotal += I2c._start();
-  errTotal += I2c._sendAddress((this->address<<1)+1);       //left shifted with R bit (1)
-  errTotal += I2c._receiveByte(0xFF, &LSB);                 //demand ACK
-  errTotal += I2c._receiveByte(0xFF, &MSB);                 //demand ACK
-  errTotal += I2c._receiveByte(0x00, &PEC);                 //last byte; don't demand ACK
-  errTotal += I2c._stop();
-  if (errTotal != 0){
-    Serial.print("Error Reading from Sensor at Address: ");
-    Serial.println(this->address, HEX);
-    return -1;                                          /////////////addition needed
+  while (errCount < 3 && errTotal != 0) {
+    errTotal = 0;
+    errTotal += I2c._start();
+    errTotal += I2c._sendAddress(this->address<<1);              //left shifted with W bit (0)
+    errTotal += I2c._sendByte(registerAddy);                  
+    errTotal += I2c._start();
+    errTotal += I2c._sendAddress((this->address<<1)+1);       //left shifted with R bit (1)
+    errTotal += I2c._receiveByte(0xFF, &LSB);                 //demand ACK
+    errTotal += I2c._receiveByte(0xFF, &MSB);                 //demand ACK
+    errTotal += I2c._receiveByte(0x00, &PEC);                 //last byte; don't demand ACK
+    errTotal += I2c._stop();
+    if (errTotal != 0){
+      errCount += 1;
+    }
+  }
+  if (errCount ==3){
+    Serial.print("Error reading from sensor with address: ");
+    Serial.println(address, HEX);
+    Serial.println("Try resetting and ensuring all connections are secure");
+    Serial.println("If problem persists, replace hardware or call a technician");
+    delay(10000000000000000000000);                           //stop running program if sensor is not working properly
   }
   else{
     int result = MSB << 8;
     result |= LSB;
-    result = ((result*.02 - 273)*9/5+32)*100;
+    result = ((result*.02 - 273.15)*9/5+32)*100;
     return result;
   }
 }
